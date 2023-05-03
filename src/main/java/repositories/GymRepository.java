0127400fb3f74c592358778096a45f8898298aa2
juan <juan@DESKTOP-UIEAVPJ>
2023-05-03 02:02:43 +0200

    configurando gymRepository

--- src/main/java/repositories/GymRepository.java
+++ src/main/java/repositories/GymRepository.java
@@ -1,6 +1,9 @@
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Gym;
@@ -8,4 +11,9 @@ import domain.Gym;
 @Repository
 public interface GymRepository extends JpaRepository<Gym, Integer>{
 
+	@Query("select m.gyms from Manager m where m.userAccount_id = ?1")
+	Collection<Gym> findByUserAccountManagerId(int UserAccountId);
+	
+	@Query("select t.gyms from Trainer t where t.userAccount_id = ?1")
+	Collection<Gym> findByUserAccountTrainerId(int UserAccountId);
 }
