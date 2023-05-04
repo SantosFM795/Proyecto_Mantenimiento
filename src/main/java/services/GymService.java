f81f4d5a3986b957ca1301ff45c2723f04f6b4c2
juan <juan@DESKTOP-UIEAVPJ>
2023-05-04 17:49:55 +0200

    creando algunos servicios

---
+++ src/main/java/services/GymService.java
@@ -0,0 +1,19 @@
+package services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class GymService {
+
+	// Managed repository -------------------------
+	
+	// Supporting services ------------------------
+	
+	// Constructor --------------------------------
+	
+	//Simple CRUD methods -------------------------
+	
+	//Other business methods ----------------------
+}
