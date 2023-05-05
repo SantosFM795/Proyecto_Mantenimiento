a6ff752c2e7a7ba62fe285b09275c29b6f5d7a30
juan <juan@DESKTOP-UIEAVPJ>
2023-05-05 17:46:01 +0200

    cv repository y service

--- src/main/java/repositories/CVRepository.java
+++ src/main/java/repositories/CVRepository.java
@@ -1,6 +1,9 @@
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.CV;
@@ -8,4 +11,6 @@ import domain.CV;
 @Repository
 public interface CVRepository extends JpaRepository<CV, Integer>{
 
+	@Query("select t.cv from Trainer t where t.userAccount_id = ?1")
+	CV findByTrainerId(int userAccountId);
 }
---
+++ src/main/java/services/CVService.java
@@ -0,0 +1,57 @@
+package services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import domain.CV;
+import repositories.CVRepository;
+
+@Service
+@Transactional
+public class CVService {
+	
+	// Managed repository -------------------------
+	
+		@Autowired
+		private CVRepository cvRepository;
+		
+		// Supporting services ------------------------
+		
+		// Constructor --------------------------------
+		
+		public CVService() {
+			super();
+		}
+		
+		//Simple CRUD methods -------------------------
+		
+		//LAS ACTIVIDADES NO SE PUEDEN CREAR NI BORRAR
+		
+		public Collection<CV> findAll(){
+			Collection<CV> result;
+			
+			Assert.notNull(this.cvRepository);
+			result=this.cvRepository.findAll();
+			Assert.notNull(result);
+			
+			return result;
+		}
+		
+		
+		//Other business methods ----------------------
+		
+		public CV findByTrainer (int trainerId) {
+			Assert.isTrue(trainerId != 0);
+			
+			CV result;
+			
+			result=cvRepository.findByTrainerId(trainerId);
+			
+			return result;
+		}
+
+}
