b5fc433f81ad4b1a12f89bde4b8420cbd2b42007
juan <juan@DESKTOP-UIEAVPJ>
2023-05-06 20:24:49 +0200

    añadiendo costumer service

--- src/main/java/repositories/CustomerRepository.java
+++ src/main/java/repositories/CustomerRepository.java
@@ -10,13 +10,10 @@ import domain.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-	
-	@Query("select c from Customer c where c.userAccount.id = ?1")
-	Customer findByUserAccountId(int userAccountId);
-	
+		
 	@Query("select s.customer from Signing s where s.gym.id = ?1")
 	Collection<Customer> findByGymId(int gymId);
 	
 	@Query("select a.customers from Activity a where a.id = ?1")
-	Customer findByActivitytId(int activityId);
+	Collection<Customer> findByActivityId(int activityId);
 }
--- src/main/java/services/CustomerService.java
+++ src/main/java/services/CustomerService.java
@@ -1,7 +1,15 @@
 package services;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import domain.Customer;
+import domain.Gym;
+import repositories.CustomerRepository;
 
 @Service
 @Transactional
@@ -9,11 +17,83 @@ public class CustomerService {
 
 	// Managed repository -------------------------
 	
-	// Supporting services ------------------------
-	
-	// Constructor --------------------------------
-	
-	//Simple CRUD methods -------------------------
-	
-	//Other business methods ----------------------
+		@Autowired
+		private CustomerRepository customerRepository;
+		
+		// Supporting services ------------------------
+		
+		// Constructor --------------------------------
+		
+		public CustomerService() {
+			super();
+		}
+		
+		//Simple CRUD methods -------------------------
+		
+		public Collection<Customer> findAll(){
+			Collection<Customer> result;
+			
+			Assert.notNull(this.customerRepository);
+			result=this.customerRepository.findAll();
+			Assert.notNull(result);
+			
+			return result;
+		}
+		
+		public Customer findOne(final int customerId) {
+			Customer result;
+			
+			result=this.customerRepository.findOne(customerId);
+			
+			return result;
+		}
+		
+		public Customer create() {
+			Customer result;
+			
+			result = new Customer();
+			
+			return result;
+		}
+		
+		public Customer save(Customer customer) {
+			Assert.notNull(customer);
+			
+			Customer result;
+			
+			result = customerRepository.save(customer);
+			
+			return result;
+		}
+		
+		public void delete (Customer customer) {
+			Assert.notNull(customer);
+			
+			Assert.isTrue(customer.getId() != 0);
+			
+			customerRepository.delete(customer);
+		}
+		
+		//Other business methods ----------------------
+		
+		
+		public Collection<Customer> findByGym(int gymId){
+			Assert.isTrue(gymId != 0);
+			
+			Collection<Customer> result;
+			
+			result=customerRepository.findByGymId(gymId);
+			
+			return result;	
+		}
+
+		public Collection<Customer> findByActivity(int activityId){
+			Assert.isTrue(activityId != 0);
+			
+			Collection<Customer> result;
+			
+			result=customerRepository.findByActivityId(activityId);
+			
+			return result;	
+		}		
 }
