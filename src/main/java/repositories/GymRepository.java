d9e3681c5002da23150c08085e78861126b3ce1d
santos <santos@DESKTOP-UIEAVPJ>
2023-05-16 17:16:52 +0200

    Cambios varios en querys

--- src/main/java/repositories/GymRepository.java
+++ src/main/java/repositories/GymRepository.java
@@ -21,14 +21,15 @@ public interface GymRepository extends JpaRepository<Gym, Integer>{
 	@Query("select s.gym from Signing s where s.customer.userAccount.id = ?1 AND s.gym.active=true")
 	Collection<Gym> findByCustomer(int UserAccountId);
 
-	/*
+	
 	@Query("select min(m.gyms.size), avg(m.gyms.size), max(m.gyms.size), stddev(m.gyms.size) from Manager m")
 	Collection<Object[]> findGymsByManager(); 
 	
-	@Query("select min(mc.gyms.size), avg(c.gyms.size), max(c.gyms.size), stddev(c.gyms.size) from Customer c")
-	Collection<Object[]> findGymsByCustomer();
+	/*
+	@Query("select min(s.gym), avg(s.gym), max(s.gym), stddev(s.gym) from Signing s where s.leaving_date=''")
+	Collection<Object[]> findGymsBySigning();
 	
-	@Query("select g1 from Gym g1 where (select count(a1) from Gym g join g.activity a1 where a1.cancelled=false and g1=g) >= ALL(select count(a2) from Gym g2 join g2.activity a2 where a2.cancelled=false group by g2")
+	
+	@Query("select g1 from Gym g1 where (select count(a1) from Gym g inner join g.activity a1 where a1.cancelled=false and g1=g) >= ALL(select count(a2) from Gym g2 inner join g2.activity a2 where a2.cancelled=false group by g2")
 	Collection<Gym> findMaxActivitiesGym();*/
-	
 }
